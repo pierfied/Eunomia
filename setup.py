b8fbf5cf59c466c915a201bc7056bcece0cdaf5a
pierfied <pierfied@email.arizona.edu>
2018-05-31 14:50:59 -0500

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='eunomia',
+    version='0.1',
+    packages=['eunomia', 'eunomia.sim_tools'],
+    url='https://github.com/pierfied/eunomia',
+    license='',
+    author='Pier Fiedorowicz',
+    author_email='pierfied@email.arizona.edu',
+    description=''
+)
